5d2038e3eb58c689ed885c55f2f8d3111271dc4b
Minh Tien Vu <minh-tien.vu@iriscorporate.com>
2022-07-12 16:14:09 +0200

    Add VirtualizationImages sample project

---
+++ samples/VirtualizationImages/ViewModels/PageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using MiniMvvm;
+
+namespace VirtualizationImages.ViewModels;
+
+public class PageViewModel : ViewModelBase
+{
+    private string _prefix;
+    private int _index;
+    private double _height = double.NaN;
+
+    public PageViewModel(int index, string prefix = "Item")
+    {
+        _prefix = prefix;
+        _index = index;
+    }
+
+    public string Header => $"{_prefix} {_index}";
+
+    public double Height
+    {
+        get => _height;
+        set => this.RaiseAndSetIfChanged(ref _height, value);
+    }
+
+
+    public byte[] DisplayImage
+    {
+        get
+        {
+            string path = "image.png";
+            return File.ReadAllBytes(path);
+        }
+        set
+        {
+            
+        }
+    }
+}
